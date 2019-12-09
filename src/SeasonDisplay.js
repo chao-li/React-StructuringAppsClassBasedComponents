9619d4e5f53a8580b8c9ffcede931b2280cb7cb7
Chao <chaoli@Chaos-MacBook-Pro.local>
2019-12-10 10:06:16 +1100

    Getting GPS Location

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const SeasonDisplay = () => {
-    return <div> Season Display</div>
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position), // success callback
+        (err) => console.log(err)
+    );
 
+    return <div> Season Display</div>
 };
 
 // exports this file and allow access by other components
