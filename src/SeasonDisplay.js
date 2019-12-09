4cdb4d6424ac386f0a1cc4a45eb922cffbd8d455
Chao <chaoli@Chaos-MacBook-Pro.local>
2019-12-10 09:56:32 +1100

    Structuring Apps with Class Based Components - init

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SeasonDisplay = () => {
+    return <div> Season Display</div>
+
+};
+
+// exports this file and allow access by other components
+export default SeasonDisplay;
