532ed547f80558c93f783d63352d0b9d94cb0e78
Chao <chaoli@Chaos-MacBook-Pro.local>
2019-12-10 10:18:00 +1100

    Refactoring Function to Class Components

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,11 +1,6 @@
 import React from 'react';
 
 const SeasonDisplay = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position), // success callback
-        (err) => console.log(err)
-    );
-
     return <div> Season Display</div>
 };
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position), // success callback
-        (err) => console.log(err)
-    );
-    return <div> testing</div>
-};
+class App extends React.Component{
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position), // success callback
+            (err) => console.log(err)
+        );
+        
+        return <div>Latitude: </div>;
+    }
+}
 
 ReactDOM.render(
     <App />, document.querySelector('#root')
