1e23213c043744eeb4022a9dd0fc57cec1118d29
Chao <chaoli@Chaos-MacBook-Pro.local>
2019-12-11 10:11:45 +1100

    Updating State Properties

--- src/index.js
+++ src/index.js
@@ -5,18 +5,21 @@ class App extends React.Component{
     constructor(props) {
         super(props); // reference to parent constructor function
         
+        // THIS IS THE ONLY TIME WE DO DIRECT ASSIGNMENT TO this.state
         this.state = {lat: null};
 
-
-    }
-    
-    render() {
+        // Get user's current location
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position), // success callback
+            (position) => {
+                // Setting State
+                this.setState({lat: position.coords.latitude});
+            },
             (err) => console.log(err)
         );
-        
-        return <div>Latitude: </div>;
+    }
+    
+    render() { 
+        return <div>Latitude: {this.state.lat}</div>;
     }
 }
 
